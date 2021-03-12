3525ac5a61f490b024c9b0dfd29ec2fd556ba1dc
rashidmajeed <rm.itprofessionalindk@gmail.com>
2021-03-13 00:30:46 +0100

    Initial Setup of project along with change in Bottom tab navigator

---
+++ types.tsx
@@ -0,0 +1,19 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Comming_Soon: undefined;
+  Search: undefined;
+  Downloads: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
